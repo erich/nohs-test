ba1362fd40c41ca3ca12dbbd6330373d8d59b0d3
Erich Kaderka <erich.kaderka@gmail.com>
2013-01-24 09:18:06 +0000

    Implemented acceptance tests in one file -> messy code to refactor

--- checkout_spec.rb
+++ checkout_spec.rb
@@ -2,11 +2,93 @@ require 'minitest/spec'
 require 'minitest/autorun'
 
 class PromotionalCode
+  attr_accessor :total_amount_before_reduction, :items
+
+  def over_amount
+    60
+  end
+
+  def percentage_reduction(by_how_many_percents = 10)
+    total_amount_before_reduction - ((total_amount_before_reduction.to_f / 100) * by_how_many_percents)
+  end
+
+  def total_price_after_percentage_reduction 
+    return total_amount_before_reduction if total_amount_before_reduction <= over_amount
+    percentage_reduction
+  end
+
+  def product_code_for_reduced_item
+    0001
+  end
+
+  def price_for_reduced_item
+    8.50
+  end
+
+  def change_price_for_item(item)
+    item.price = price_for_reduced_item
+    item
+  end
+
+  def total_price_after_item_price_reduction
+    select_items_for_reduction.map {|item| change_price_for_item(item) } if select_items_for_reduction.size > 1
+    items
+  end
+
+  private
+  def select_items_for_reduction
+    items.select {|item| item.product_code == product_code_for_reduced_item}
+  end
+end
+
+describe PromotionalCode do
+  
+  subject { PromotionalCode.new }
+
+  it 'has total_amount' do
+    subject.respond_to?(:total_amount_before_reduction).must_equal true
+  end
+
+  it 'has percentage_reduction' do
+    subject.respond_to?(:percentage_reduction).must_equal true
+  end
+
+  it 'reduces price if total is over 60 by 10%' do 
+    subject.total_amount_before_reduction = 70
+    subject.total_price_after_percentage_reduction.must_equal 63
+  end
+
+  it 'doesn\'t reduce price if total is 60 or under 60' do
+    subject.total_amount_before_reduction = 60
+    subject.total_price_after_percentage_reduction.must_equal 60
+    subject.total_amount_before_reduction = 59
+    subject.total_price_after_percentage_reduction.must_equal 59
+  end
+  
+  it 'changes price pro item' do
+    item = Item.new(product_code: 0001, price: 10)
+    subject.change_price_for_item(item).price.must_equal 8.50
+  end
+
+  it 'reduces price if there are two or more items with reduced price' do
+    item = Item.new(product_code: 0001, price: 10)
+    subject.items = [item, item]
+    subject.total_price_after_item_price_reduction.first.price.must_equal 8.50
+    subject.total_price_after_item_price_reduction.last.price.must_equal 8.50
+  end
+
+  it 'doesn\'t reduce price if there is only one item with reduced price' do
+    item = Item.new(product_code: 0001, price: 10)
+    subject.items = [item]
+    subject.total_price_after_item_price_reduction.first.price.must_equal 10
+  end
+
 end
 
 class Item
 
-  attr_reader :product_code, :name, :price
+  attr_reader :product_code, :name
+  attr_accessor :price
 
   def initialize(hash_values)
     @product_code = hash_values[:product_code]
@@ -53,11 +135,18 @@ class Checkout
   end
   
   def total
-    "£#{calculate_total_price}"
+    "£#{round_calculated_total_price}"
   end
 
   def calculate_total_price
-    scanned_items.collect(&:price).inject(:+)
+    promotional_code.items = scanned_items
+    promotional_code.total_price_after_item_price_reduction
+    promotional_code.total_amount_before_reduction = scanned_items.collect(&:price).inject(:+)
+    promotional_code.total_price_after_percentage_reduction
+  end
+
+  def round_calculated_total_price
+    calculate_total_price.round(2)
   end
   
 end
